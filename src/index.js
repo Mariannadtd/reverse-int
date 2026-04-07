405d74a2b690c0493c2afb36be3ce898ee0dea3e
Mariannadtd <romanova.lenovo@gmail.com>
2026-04-07 22:39:31 +0300

    feat: implement reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(number) {
+  const str = Math.abs(number).toString();
+  let res = '';
+
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    res += str[i];
+  }
+
+  return Number(res);
 };
